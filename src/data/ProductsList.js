e14851425cf9bd16199157555f6d0cbc51401bf5
sh.alimardani <shadialimardani07@gmail.com>
2023-10-02 18:19:08 +0330

    test git

---
+++ src/data/ProductsList.js
@@ -0,0 +1,8 @@
+const productsList =[
+    {id:1 , name:'iphone 13' , price:2000},
+    {id:2 , name:'iphone 12' , price:3000},
+    {id:3 , name:'iphone 11' , price:4000},
+    {id:4 , name:'iphone 14' , price:5000},
+]
+
+export default productsList
